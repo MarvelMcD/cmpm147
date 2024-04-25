00e65fe3d8409441714145364ce9de64e68e6f7d
MarvelMcD <MarvelMcD@gmail.com>
2024-04-24 23:21:36 -0700

    update

--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -1,7 +1,7 @@
 // sketch.js - purpose and description here
 // Author: Marvel McDowell
-// Collaborators Chat GPT and Romen Luo 
-// Date: 4/23/24
+// Collaborators Chat GPT and Romen Luo
+// Date:
 
 // Here is how you might set up an OOP p5.js project
 // Note that p5.js looks for a file called sketch.js
