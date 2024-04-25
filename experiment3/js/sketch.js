02958ee9995ad2202bebef922301ca3b5c71734c
MarvelMcD <MarvelMcD@gmail.com>
2024-04-24 23:18:56 -0700

    Update sketch.js

--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -1,91 +1,89 @@
 // sketch.js - purpose and description here
 // Author: Marvel McDowell
-//Collaborators Romen Luo
-// Date:
+// Collaborators Chat GPT and Romen Luo 
+// Date: 4/23/24
 
 // Here is how you might set up an OOP p5.js project
 // Note that p5.js looks for a file called sketch.js
 
-// function resizeScreen() {
-//   centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
-//   centerVert = canvasContainer.height() / 2; // Adjusted for drawing logic
-//   console.log("Resizing...");
-//   resizeCanvas(canvasContainer.width(), canvasContainer.height());
-//   // redrawCanvas(); // Redraw everything based on new size
-// }
+function resizeScreen() {
+  centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
+  centerVert = canvasContainer.height() / 2; // Adjusted for drawing logic
+  console.log("Resizing...");
+  resizeCanvas(canvasContainer.width(), canvasContainer.height());
+  // redrawCanvas(); // Redraw everything based on new size
+}
 
-// let seed = 0;
-// let tilesetImage;
-// let currentGrid = [];
-// let numRows, numCols;
+let seed = 0;
+let tilesetImage;
+let currentGrid = [];
+let numRows, numCols;
 
-// function preload() {
-//   tilesetImage = loadImage(
-//     "https://cdn.glitch.com/25101045-29e2-407a-894c-e0243cd8c7c6%2FtilesetP8.png?v=1611654020438"
-//   );
-// }
+function preload() {
+  tilesetImage = loadImage(
+    "https://cdn.glitch.com/25101045-29e2-407a-894c-e0243cd8c7c6%2FtilesetP8.png?v=1611654020438"
+  );
+}
 
-// function reseed() {
-//   seed = (seed | 0) + 1109;
-//   randomSeed(seed);
-//   noiseSeed(seed);
-//   select("#seedReport").html("seed " + seed);
-//   regenerateGrid();
-// }
+function reseed() {
+  seed = (seed | 0) + 1109;
+  randomSeed(seed);
+  noiseSeed(seed);
+  select("#seedReport").html("seed " + seed);
+  regenerateGrid();
+}
 
-// function regenerateGrid() {
-//   select("#canvasContainer").value(gridToString(generateGrid(numCols, numRows)));
-//   reparseGrid();
-// }
+function regenerateGrid() {
+  select("#canvasContainer").value(gridToString(generateGrid(numCols, numRows)));
+  reparseGrid();
+}
 
-// function reparseGrid() {
-//   currentGrid = stringToGrid(select("#canvasContainer").value());
-// }
+function reparseGrid() {
+  currentGrid = stringToGrid(select("#canvasContainer").value());
+}
 
-// function gridToString(grid) {
-//   let rows = [];
-//   for (let i = 0; i < grid.length; i++) {
-//     rows.push(grid[i].join(""));
-//   }
-//   return rows.join("\n");
-// }
+function gridToString(grid) {
+  let rows = [];
+  for (let i = 0; i < grid.length; i++) {
+    rows.push(grid[i].join(""));
+  }
+  return rows.join("\n");
+}
 
-// function stringToGrid(str) {
-//   let grid = [];
-//   let lines = str.split("\n");
-//   for (let i = 0; i < lines.length; i++) {
-//     let row = [];
-//     let chars = lines[i].split("");
-//     for (let j = 0; j < chars.length; j++) {
-//       row.push(chars[j]);
-//     }
-//     grid.push(row);
-//   }
-//   return grid;
-// }
+function stringToGrid(str) {
+  let grid = [];
+  let lines = str.split("\n");
+  for (let i = 0; i < lines.length; i++) {
+    let row = [];
+    let chars = lines[i].split("");
+    for (let j = 0; j < chars.length; j++) {
+      row.push(chars[j]);
+    }
+    grid.push(row);
+  }
+  return grid;
+}
 
-// function setup() {
-//   numCols = select("#canvasContainer").attribute("rows") | 0;
-//   numRows = select("#canvasContainer").attribute("cols") | 0;
+function setup() {
+  numCols = select("#canvasContainer").attribute("rows") | 0;
+  numRows = select("#canvasContainer").attribute("cols") | 0;
 
-//   createCanvas(16 * numCols, 16 * numRows).parent("canvasContainer");
-//   select("canvas").elt.getContext("2d").imageSmoothingEnabled = false;
+  createCanvas(16 * numCols, 16 * numRows).parent("canvasContainer");
+  select("canvas").elt.getContext("2d").imageSmoothingEnabled = false;
 
-//   select("#reseedButton").mousePressed(reseed);
+  select("#reseedButton").mousePressed(reseed);
 
-//   reseed();
-// }
+  reseed();
+}
 
-// function draw() {
-//   randomSeed(seed);
-//   drawGrid(currentGrid);
-// }
+function draw() {
+  randomSeed(seed);
+  drawGrid(currentGrid);
+}
 
-// function placeTile(i, j, ti, tj) {
-//   image(tilesetImage, 16 * j, 16 * i, 16, 16, 8 * ti, 8 * tj, 8, 8);
-// }
-
-////////////////////////////////////////
+function placeTile(i, j, ti, tj) {
+  image(tilesetImage, 16 * j, 16 * i, 16, 16, 8 * ti, 8 * tj, 8, 8);
+}
 
 // function generateGrid(numCols, numRows) {
 //   let grid = [];
@@ -133,89 +131,6 @@
 //     }
 //   }
 // }
-///////////////////////////////////////////////////////////
-var s = function( sketch ) {
-
-  function resizeScreen() {
-    centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
-    centerVert = canvasContainer.height() / 2; // Adjusted for drawing logic
-    console.log("Resizing...");
-    resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    // redrawCanvas(); // Redraw everything based on new size
-  }
-  
-  let seed = 0;
-  let tilesetImage;
-  let currentGrid = [];
-  let numRows, numCols;
-  
-  function preload() {
-    tilesetImage = loadImage(
-      "https://cdn.glitch.com/25101045-29e2-407a-894c-e0243cd8c7c6%2FtilesetP8.png?v=1611654020438"
-    );
-  }
-  
-  function reseed() {
-    seed = (seed | 0) + 1109;
-    randomSeed(seed);
-    noiseSeed(seed);
-    select("#seedReport").html("seed " + seed);
-    regenerateGrid();
-  }
-  
-  function regenerateGrid() {
-    select("#canvasContainer").value(gridToString(generateGrid(numCols, numRows)));
-    reparseGrid();
-  }
-  
-  function reparseGrid() {
-    currentGrid = stringToGrid(select("#canvasContainer").value());
-  }
-  
-  function gridToString(grid) {
-    let rows = [];
-    for (let i = 0; i < grid.length; i++) {
-      rows.push(grid[i].join(""));
-    }
-    return rows.join("\n");
-  }
-  
-  function stringToGrid(str) {
-    let grid = [];
-    let lines = str.split("\n");
-    for (let i = 0; i < lines.length; i++) {
-      let row = [];
-      let chars = lines[i].split("");
-      for (let j = 0; j < chars.length; j++) {
-        row.push(chars[j]);
-      }
-      grid.push(row);
-    }
-    return grid;
-  }
-  
-  function setup() {
-    numCols = select("#canvasContainer").attribute("rows") | 0;
-    numRows = select("#canvasContainer").attribute("cols") | 0;
-  
-    createCanvas(16 * numCols, 16 * numRows).parent("canvasContainer");
-    select("canvas").elt.getContext("2d").imageSmoothingEnabled = false;
-  
-    select("#reseedButton").mousePressed(reseed);
-  
-    reseed();
-  }
-  
-  function draw() {
-    randomSeed(seed);
-    drawGrid(currentGrid);
-  }
-  
-  function placeTile(i, j, ti, tj) {
-    image(tilesetImage, 16 * j, 16 * i, 16, 16, 8 * ti, 8 * tj, 8, 8);
-  }
-
-  ///////////////////
 
 function generateGrid(numCols, numRows) {
   let grid = [];
@@ -379,138 +294,4 @@ const lookup = [
   [0, 0]  // 1111
 ];
 
-};
-
-var myp5_1 = new p5(s, 'p5sketch');
-
-var s2 = function( sketch ){
-  function resizeScreen() {
-    centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
-    centerVert = canvasContainer.height() / 2; // Adjusted for drawing logic
-    console.log("Resizing...");
-    resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    // redrawCanvas(); // Redraw everything based on new size
-  }
-  
-  let seed = 0;
-  let tilesetImage;
-  let currentGrid = [];
-  let numRows, numCols;
-  
-  function preload() {
-    tilesetImage = loadImage(
-      "https://cdn.glitch.com/25101045-29e2-407a-894c-e0243cd8c7c6%2FtilesetP8.png?v=1611654020438"
-    );
-  }
-  
-  function reseed() {
-    seed = (seed | 0) + 1109;
-    randomSeed(seed);
-    noiseSeed(seed);
-    select("#seedReport").html("seed " + seed);
-    regenerateGrid();
-  }
-  
-  function regenerateGrid() {
-    select("#canvasContainer").value(gridToString(generateGrid(numCols, numRows)));
-    reparseGrid();
-  }
-  
-  function reparseGrid() {
-    currentGrid = stringToGrid(select("#canvasContainer").value());
-  }
-  
-  function gridToString(grid) {
-    let rows = [];
-    for (let i = 0; i < grid.length; i++) {
-      rows.push(grid[i].join(""));
-    }
-    return rows.join("\n");
-  }
-  
-  function stringToGrid(str) {
-    let grid = [];
-    let lines = str.split("\n");
-    for (let i = 0; i < lines.length; i++) {
-      let row = [];
-      let chars = lines[i].split("");
-      for (let j = 0; j < chars.length; j++) {
-        row.push(chars[j]);
-      }
-      grid.push(row);
-    }
-    return grid;
-  }
-  
-  function setup() {
-    numCols = select("#canvasContainer").attribute("rows") | 0;
-    numRows = select("#canvasContainer").attribute("cols") | 0;
-  
-    createCanvas(16 * numCols, 16 * numRows).parent("canvasContainer");
-    select("canvas").elt.getContext("2d").imageSmoothingEnabled = false;
-  
-    select("#reseedButton").mousePressed(reseed);
-  
-    reseed();
-  }
-  
-  function draw() {
-    randomSeed(seed);
-    drawGrid(currentGrid);
-  }
-  
-  function placeTile(i, j, ti, tj) {
-    image(tilesetImage, 16 * j, 16 * i, 16, 16, 8 * ti, 8 * tj, 8, 8);
-  }
-
-  //////////////////////
-function generateGrid(numCols, numRows) {
-  let grid = [];
-  for (let i = 0; i < numRows; i++) {
-    let row = [];
-    for (let j = 0; j < numCols; j++) {
-      row.push("_");
-    }
-    grid.push(row);
-  }
-
-
-  for (let i = 0; i < numRows; i++) {
-    for (let j = 0; j < numCols; j++) {
-      if (random() < 0.05) {
-        grid[i][j] = ".";
-      }
-    }
-  }
-  
-  for (let i = 0; i < numRows; i++) {
-    for (let j = 0; j < numCols; j++) {
-      if (random() < 0.01) {
-        grid[i][j] = "/";
-      }
-    }
-  }
-
-  return grid;
-}
-
-
-function drawGrid(grid) {
-  background(128);
-
-  for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid[i].length; j++) {
-      if (grid[i][j] == '_') {
-        placeTile(i, j, floor(random(4)), 3);
-      } else if (grid[i][j] == '.') {
-        placeTile(i, j, floor(random(4)), 10); 
-      } else if (grid[i][j] == '/') {
-        placeTile(i, j, 0, 10); 
-      }
-    }
-  }
-} 
-};
-
-var myp5_2 = new p5(s2, 'p5sketch');
 
